590513d0f8c2180f6003e9b0fede5da273624270
Luis Alberto Mesili <42626226+Guitarra2428@users.noreply.github.com>
2024-10-29 16:03:40 -0400

    Add files via upload

---
+++ PracticaSRP/PracticaSRP/Persona.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaSRP
+{
+    public class Persona
+    {
+        //Refactorisado
+        //private string Nombre;
+        //private int Edad;
+        //private string Direccion;
+        //private string CorreoElectronico;
+
+        //public  void  EnviarCorreoElectronico(string mensaje)
+        //{
+
+        //} 
+
+        //public  void  ImpreimirData()
+        //{
+        //    Console.WriteLine($"Nombre:{ Nombre}");
+        //    Console.WriteLine($"Edad:{Edad}");
+        //    Console.WriteLine($"Direccion:{Direccion}");
+        //    Console.WriteLine($"CorreoElectronico:{CorreoElectronico}");
+        //}
+
+        private string Nombre;
+        private int Edad;
+        private string Direccion;
+        private string CorreoElectronico;
+
+     
+        public void ImpreimirData()
+        {
+            Console.WriteLine($"Nombre:{Nombre}");
+            Console.WriteLine($"Edad:{Edad}");
+            Console.WriteLine($"Direccion:{Direccion}");
+            Console.WriteLine($"CorreoElectronico:{CorreoElectronico}");
+        }
+    }
+
+    public class Correo
+    {
+        public void EnviarCorreoElectronico(string mensaje)
+        {
+
+        }       
+
+
+    }
+}
